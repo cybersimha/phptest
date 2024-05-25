db6b670ed4f65a51129772b39a8813002c0b96d5
cybersimha <119353028+cybersimha@users.noreply.github.com>
2024-05-25 17:30:21 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,10 +1,14 @@
 <?php
 $conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL); // Enable SSL
-$conn = mysqli_real_connect($conn, "mydatabasetest.mysql.database.azure.com", "mysqldbadmin", "P@ssword1234", "mydatabase", 3306, MYSQLI_CLIENT_SSL);
+$connString = getenv('MYSQL_CONN_STRING');
+
+// Use $connString to connect to your MySQL database
+$conn = new mysqli($connString);
+
+
 
 if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    echo "*******Failed to connect to MySQL: " . mysqli_connect_error();
 } else {
 	echo "connection successful";
     // Code to interact with your database
