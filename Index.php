6c0dac6ab484690a1ca0840df8d446fd8e22aa8f
cybersimha <119353028+cybersimha@users.noreply.github.com>
2024-05-25 17:42:59 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -4,4 +4,16 @@ $connString = getenv('MYSQL_CONN_STRING');
 
 echo $connString;
 
+// Use $connString to connect to your MySQL database
+$conn = new mysqli($connString);
+
+
+
+if (mysqli_connect_errno()) {
+    echo "*******Failed to connect to MySQL: " . mysqli_connect_error();
+} else {
+	echo "connection successful";
+    // Code to interact with your database
+}
+
 ?>
